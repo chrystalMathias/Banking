db64c4d0fd1ec585a4eb17e68567640045a26d23
Kushagravaibhaw <kushagravaibhaw6@gmail.com>
2023-02-21 23:11:49 +0530

    final commit

--- app/src/main/java/com/example/bank/customers.java
+++ app/src/main/java/com/example/bank/customers.java
@@ -12,7 +12,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class customers extends AppCompatActivity {
-    Button gopal,sunaina,somya,suraj,ritu,mohit,krishna,sonu,richa,sinha;
+    Button mohan,govind,rohit,virat,ritu,mohit,sharavya,sonu,richa,sinha;
     SQLiteDatabase db;
 
     @SuppressLint("MissingInflatedId")
@@ -20,26 +20,26 @@ public class customers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customers);
-        gopal=findViewById(R.id.gopal);
-        sunaina=findViewById(R.id.sunaina);
-        somya=findViewById(R.id.somya);
-        suraj=findViewById(R.id.suraj);
+        mohan=findViewById(R.id.gopal);
+        govind=findViewById(R.id.sunaina);
+        rohit=findViewById(R.id.somya);
+        virat=findViewById(R.id.suraj);
         ritu=findViewById(R.id.ritu);
         mohit=findViewById(R.id.mohit);
-        krishna=findViewById(R.id.krishna);
+        sharavya=findViewById(R.id.krishna);
         sonu=findViewById(R.id.sonu);
         richa=findViewById(R.id.richa);
         sinha=findViewById(R.id.sinha);
         db=openOrCreateDatabase("customers",MODE_PRIVATE,null);
 
 
-        gopal.setOnClickListener(new View.OnClickListener() {
+        mohan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i4=new Intent(customers.this,info.class);
                 db.execSQL("create table if not exists customer(name varchar(10) ,Email varchar(20),accno varchar(20),adress varchar(20),current_balance varchar(20))");
-                String name="gopal singh";
-                String Email="gopalsingh2@gmail.com";
+                String name="mohan singh";
+                String Email="mohansinghss2@gmail.com";
                 String adress="ranagar sector 6 bhilai chattisgargh";
                 String num="";
                 int current_balance=500;
@@ -57,14 +57,14 @@ public class customers extends AppCompatActivity {
 
             }
         });
-        sunaina.setOnClickListener(new View.OnClickListener() {
+        govind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i4=new Intent(customers.this,info.class);
 
                 db.execSQL("create table if not exists customer(name varchar(10) ,Email varchar(20),accno varchar(20),adress varchar(20),current_balance varchar(20))");
-                String name="sunaina singh";
-                String Email="kauravneet6@gmail.com";
+                String name="govind singh";
+                String Email="govindsinghs6@gmail.com";
                 String adress="Rajiv road line 61 ludiana,punjab";
                 String num="";
                 int current_balance=1000;
@@ -87,13 +87,13 @@ public class customers extends AppCompatActivity {
         db=openOrCreateDatabase("customer",MODE_PRIVATE,null);
 
 
-        somya.setOnClickListener(new View.OnClickListener() {
+        rohit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i4=new Intent(customers.this,info.class);
                 db.execSQL("create table if not exists customer(name varchar(10) ,Email varchar(20),accno varchar(20),adress varchar(20),current_balance varchar(20))");
-                String name="somya singh";
-                String Email="somyasingh42@gmail.com";
+                String name="rohit sharma";
+                String Email="rohitsharma22222222@gmail.com";
                 String adress="ranagar sector 6 bhilai chattisgargh";
                 String num="";
                 int current_balance=500;
@@ -115,13 +115,13 @@ public class customers extends AppCompatActivity {
         db=openOrCreateDatabase("customer",MODE_PRIVATE,null);
 
 
-        suraj.setOnClickListener(new View.OnClickListener() {
+        virat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i4=new Intent(customers.this,info.class);
                 db.execSQL("create table if not exists customer(name varchar(10) ,Email varchar(20),accno varchar(20),adress varchar(20),current_balance varchar(20))");
-                String name="suraj sharma";
-                String Email="surajsharma22@gmail.com";
+                String name="virat kohli";
+                String Email="virats22@gmail.com";
                 String adress="ranagar sector 6 bhilai chattisgargh";
                 String num="";
                 int current_balance=500;
@@ -192,13 +192,13 @@ public class customers extends AppCompatActivity {
 
             }
         });
-        krishna.setOnClickListener(new View.OnClickListener() {
+        sharavya.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i4=new Intent(customers.this,info.class);
                 db.execSQL("create table if not exists customer(name varchar(10) ,Email varchar(20),accno varchar(20),adress varchar(20),current_balance varchar(20))");
-                String name="krishna sharma";
-                String Email="krishnasharms22222@gmail.com";
+                String name="sharavya";
+                String Email="sharavyas22222@gmail.com";
                 String adress="ranagar sector 6 bhilai chattisgargh";
                 String num="";
                 int current_balance=500;
